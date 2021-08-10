2f48172d94ee427f782aa791f7b7f4dee1a8a400
diogolaureano <diogolaureano.sousa@gmail.com>
2021-08-09 22:37:31 -0300

    Criando formulario

--- index.js
+++ index.js
@@ -8,25 +8,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    var nome = "Diogo";
-    var lang = "Java";
-    var exibeMSG = false;
-
-    var produtos = [
-        { nome: "Doritos", preco: 3.14 },
-        { nome: "Chips", preco: 5.00 },
-        { nome: "Leite", preco: 3.99 }
-    ];
-
-    //res.send("Bem vindo ao meu site!");
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Salve",
-        inscritos: 8000,
-        msg: exibeMSG,
-        produtos: produtos
-    });
+    res.render("index")
 });
 
 app.listen(8080, () => { console.log("App rodando!") });
